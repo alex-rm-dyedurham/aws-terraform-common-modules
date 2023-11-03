f212b0a4db9d1ea2f119cde27c79e2495a611854
alex-rm-dyedurham <141664096+alex-rm-dyedurham@users.noreply.github.com>
2023-11-03 00:31:55 +0000

    outputs updated

--- modules/containers/ecr_new/outputs.tf
+++ modules/containers/ecr_new/outputs.tf
@@ -1,5 +1,5 @@
 output "ecr_repos_arn" {
-  value = zipmap(var.repo_names, module.ecr.repos.*.arn)
+  value = zipmap(var.repo_names, module.ecr.repos.*.repository_arn)
 }
 
 output "ecr_repos_url" {
