a2776be8bbb8eca2b82f34944d131ef6f339e4be
alex-rm-dyedurham <141664096+alex-rm-dyedurham@users.noreply.github.com>
2023-11-07 16:13:31 -0600

    Adding new code (#5)

--- modules/containers/ecr_new/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "ecr_repos_arn" {
-  value = zipmap(var.repo_names, module.ecr.*.repository_arn)
-}
-
-output "ecr_repos_url" {
-  value = zipmap(var.repo_names, module.ecr.*.repository_url)
-}
